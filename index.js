8fa60247fc497681178ca658d87209562c6ec1b9
Rinidh <rinidhdhokia@gmail.com>
2024-12-22 12:17:48 +0300

    Show dropdown only on hover over Products in navbar

---
+++ index.js
@@ -0,0 +1,14 @@
+// JS VERSION OF DROPDOWN ON HOVER
+
+// const nav = document.querySelector("nav")
+// const navProducts = nav.querySelector('.products')
+// const dropdown = navProducts.querySelector('.dropdown')
+
+// dropdown.style.display = 'none'
+
+// navProducts.addEventListener('mouseenter', () => {
+//   dropdown.style.display = 'block'
+// })
+// navProducts.addEventListener('mouseleave', () => {
+//   dropdown.style.display = 'none'
+// })
